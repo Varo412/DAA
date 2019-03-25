b03b557eccc036c8a9e29f4609f68952c5a75808
Álvaro Rivas <48073509+Varo412@users.noreply.github.com>
2019-03-25 18:26:20 +0100

    Update DAA_p0cDesfiles.java

--- DAA_p0cDesfiles.java
+++ DAA_p0cDesfiles.java
@@ -1,67 +1,61 @@
-package daa_p0cdesfiles;
+package desfilestemp;
 
 import java.util.ArrayList;
-import java.util.Iterator;
 import java.util.List;
 import java.util.Scanner;
 
-/**
- * @author Varo412
- */
-public class DAA_p0cDesfiles {
+public class DesfilesTemp {
 
     public static void main(String[] args) {
-        Scanner lector= new Scanner(System.in);
-        int n =lector.nextInt();    //nodos
-        int m=lector.nextInt();     //aristas
-        int padre=0;        //valor auxiliar
+        Scanner scan=new Scanner(System.in);
+        int n=scan.nextInt();   //n
+        int m=scan.nextInt();   //m
+        int pn;
         
-        List<Integer>[]g=new List[n];       //inicializo grafo
-        Boolean visited[]=new Boolean[n];
+        List<Integer>[]g=new List[n];        //creamos estructura grafo
         for(int i=0;i<n;i++){
             g[i]=new ArrayList(m);
-            visited[i]=false;
         }
         
-        for(int i=0;i<m;i++){               //relleno grafo
-            int nod1=Integer.parseInt(lector.next())-1;
-            int nod2=Integer.parseInt(lector.next())-1;
-            g[nod1].add(nod2);
-            g[nod2].add(nod1);
+        for(int i=0;i<m;i++){
+            int a=scan.nextInt()-1;
             if(i==0){
-                padre=nod1;
+                pn=a;
             }
+            int b=scan.nextInt()-1;
+            g[a].add(b);
+            g[b].add(a);
         }
-        boolean sol=Cycle(n,visited,padre,g);
-        System.out.print(sol);
-       
-        
-       
-        
-        
-        
-        
-
+        int borrar=-1;
         
         
     }
     
-    static boolean Cycle(int n, Boolean visited[],int parent, List<Integer>[]g){
-        visited[n-1]=true;
-        int i;
-        Iterator<Integer> it=g[n-1].iterator();
-        while(it.hasNext()){
-            i=it.next();
-            if(!visited[i]){
-                if(Cycle(i,visited,n-1,g)){
-                    return true;
-                }
-            }
-            else if(i!=parent){
-                return true;
-            }
+    /*public static List<Integer>[] DFS(List<Integer>[]g){
+        int n=g.length;
+        List<Integer> traversales[]=new List[n];
+        for(int v=0;v<n;v++){
+            traversales[n]=DFS(g,v);
         }
-        return false;
+        return traversales;
+    }*/
+    
+    public static List<Integer>DFS(List<Integer>[] g, int v){
+        int n=g.length;
+        List<Integer>traversal=new ArrayList<>(n);  //lista del recorrido
+        boolean [] visited=new boolean[n];          //array de visitados
+        recursiveDFS(g,v,visited,traversal);
+        return traversal;
     }
     
+    private static void recursiveDFS(List<Integer>[]g,int v, boolean[] visited,
+            List<Integer>traversal){
+        visited[v]=true;
+        traversal.add(v);
+        for(int adj:g[v]){
+            if(!visited[adj]){
+                recursiveDFS(g,adj,visited,traversal);
+            }
+        }
+    }
 }
